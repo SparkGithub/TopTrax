86c2bc3298aaeb1b1d305fef641a536c83fe01f7
SparkGithub <daniel.nadav@gmail.com>
2015-03-12 22:51:09 +0200

    Initial Commit

---
+++ TopTrax/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopTrax
+//
+//  Created by Spark on 3/12/15.
+//  Copyright (c) 2015 Spark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
